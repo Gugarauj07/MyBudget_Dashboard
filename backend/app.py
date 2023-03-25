9332be69c2f4dda2c6504bae456e92105953cf4c
Gustavo Araújo Lima <92393578+Gugarauj07@users.noreply.github.com>
2023-03-25 19:28:20 +0000

    finished controllers

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify, request
 import models
 from db import db
-from flask_smorest import Api
 
 def create_app():
 		
@@ -10,17 +9,18 @@ def create_app():
 	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 	db.init_app(app)
 
-	# api = Api(app)
-
 	with app.app_context():
 		db.create_all()
 
 	from controllers.userController import user_controller_bp
 	from controllers.categoriaController import categoria_controller_bp
 	from controllers.despesaController import despesa_controller_bp
+	from controllers.receitaController import receita_controller_bp
+
 	app.register_blueprint(user_controller_bp)
 	app.register_blueprint(categoria_controller_bp)
 	app.register_blueprint(despesa_controller_bp)
+	app.register_blueprint(receita_controller_bp)
 
 	@app.route('/')
 	def index():
