3f7fd34773dfc489ba47e185b06d071113b610e3
Gugarauj07 <gugaraujo01@gmail.com>
2023-12-27 15:18:22 -0300

    Rebuilding project

--- backend/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from models.userModel import User
-from models.categoriaModel import Categorias
-from models.despesaModel import Despesas
-from models.receitaModel import Receitas
-from models.metaModel import Metas
-from models.investimentoModel import Investimentos
