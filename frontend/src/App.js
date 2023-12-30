50e7ab60ecd7a3e5864e5cb7138235b4aef3b73d
Gugarauj07 <gugaraujo01@gmail.com>
2023-12-30 15:47:16 -0300

    History

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, {useState, useMemo} from 'react'
 import styled from "styled-components";
 import {MainLayout} from './styles/Layouts'
-import Orb from './Components/Orb/Orb'
 import Navigation from './Components/Navigation/Navigation'
 import Dashboard from './Components/Dashboard/Dashboard';
 import Income from './Components/Income/Income'
@@ -9,6 +8,25 @@ import Expenses from './Components/Expenses/Expenses';
 import { useGlobalContext } from './context/globalContext';
 
 function App() {
+  const [active, setActive] = useState(1)
+
+  const global = useGlobalContext()
+  console.log(global);
+
+  const displayData = () => {
+    switch(active){
+      case 1:
+        return <Dashboard />
+      case 2:
+        return <Dashboard />
+      case 3:
+        return <Income />
+      case 4: 
+        return <Expenses />
+      default: 
+        return <Dashboard />
+    }
+  }
 
   return (
     <AppStyled className="App">
@@ -24,7 +42,7 @@ function App() {
 
 const AppStyled = styled.div`
   height: 100vh;
-  background-color: white;
+  background-color: #fff;
   position: relative;
   main{
     flex: 1;
